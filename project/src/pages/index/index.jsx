8b3d5a0141e904a58adf742b30b052aeba94a892
骆思杰 <luo_sj@163.com>
2020-06-05 16:17:22 +0800

    use taro

---
+++ project/src/pages/index/index.jsx
@@ -0,0 +1,73 @@
+import Taro, { Component } from '@tarojs/taro'
+import { View, Button, Text } from '@tarojs/components'
+import { observer, inject } from '@tarojs/mobx'
+
+import './index.scss'
+
+
+@inject('counterStore')
+@observer
+class Index extends Component {
+
+  config = {
+    navigationBarTitleText: '图萌'
+  }
+
+  componentWillMount () { }
+
+  componentWillReact () {
+    console.log('componentWillReact')
+  }
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  increment = () => {
+    const { counterStore } = this.props
+    counterStore.increment()
+  }
+
+  decrement = () => {
+    const { counterStore } = this.props
+    counterStore.decrement()
+  }
+
+  incrementAsync = () => {
+    const { counterStore } = this.props
+    counterStore.incrementAsync()
+  }
+  navigateTo(url) {
+    // window.location.href = url
+  }
+
+  render () {
+    const { counterStore: { counter } } = this.props
+    return (
+      <View className='index'>
+        <div class="image-container">
+          <div class="add-image">
+            <i class="iconfont icon-add-image"></i>
+            <span class="label">添加图片</span>
+          </div>
+        </div>
+        <div class="menus">
+          <div class="tool add-text" onClick={this.navigateTo('/edit')}>
+            <i class="iconfont icon-text"></i>
+            <span class="label">文字</span>
+          </div>
+          <div class="tool add-sticker">
+            <i class="iconfont icon-sticker"></i>
+            <span class="label">贴纸</span>
+          </div>
+        </div>
+      </View>
+    )
+  }
+}
+
+export default Index 
